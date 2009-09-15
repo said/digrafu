8ef32ebcec82aef7f72fa91ea11e948fdb51d909
GRODias <grodias@gmail.com>
2009-09-15 11:54:08 -0300

    primeiro commit

---
+++ BasePackage/MainAbstract.pm
@@ -0,0 +1,193 @@
+	##################################################
+	## that class serves as base to classes of 	    ##
+	## methods to simplification of the main with	##
+	## the use of the polimorfism					##
+	##################################################
+
+    package AbstractMethod;
+   	use strict;
+    use warnings;
+   	use MainParameters;
+   	use File::Copy;
+
+    ##################################################
+    ## methods implementations		    			##
+    ##################################################
+	
+	## method to implementation in subclasses		##
+	
+	sub executeMethod;
+	
+	## private methods								##
+	
+	my $_dying = sub {
+		my $instance = $_[1];
+		my $tempDir = $instance->parameter("TEMP");
+		unlink <./outfile>;
+		print "DNADIST ERROR:\n";
+		system "cat $tempDir/dnadistlog | grep WARNING";
+		system "cat $tempDir/dnadistlog | grep ERROR";
+		die "";
+	};
+	
+	my $_executeDNADist = sub  {
+		
+		my $instance = $_[1];
+        my $pwd = $instance->parameter("PWD");
+        my $model = $instance->parameter("MODEL");
+        my $tempDir = $instance->parameter("TEMP");
+        my $fileIn = $instance->parameter("INPUT");
+       	my $weight = $instance->parameter("WEIGHT");
+        my $letters = $instance->parameter("LETTER");
+        my $ratio = $instance->parameter("RATIO");
+        my $gamma = $instance->parameter("GAMMA");
+        my $freque = $instance->parameter("FREQUE");
+		my $alpha = $instance->parameter("ALPHA");
+		my $fraction = $instance->parameter("ISITE");
+        
+        my $parameters = $fileIn."\n";
+        
+        my $modelValue = 0;
+		if($model =~ /^k/) {
+			$modelValue = 1;
+		}
+		elsif($model =~ /^j/) {
+			$modelValue = 2;
+		}
+		elsif($model =~ /^l/) {
+			$modelValue = 3;
+		}
+		for my $i (1..$modelValue) {
+			$parameters .= "D\n";
+		}
+		
+		if(defined $ratio && ($modelValue == 0 || $modelValue == 1)){
+			$parameters .= "T\n$ratio\n"; 
+		}
+		
+		if($modelValue != 3) {
+			for my $i(1..$gamma){
+				$parameters .= "G\n";
+			}
+		}
+		
+		if($modelValue == 0 && defined $freque && (@_ = split /,/,$freque) == 4){
+			$parameters .= "F\n".join(" ",@_)."\n";
+		}
+		
+		$parameters .= "I\n2\n";
+		
+		if(defined $weight && $weight ne "n") {
+			$parameters .= "W\n";
+			$parameters .= "Y\n";
+			
+			if($gamma > 0){
+				$parameters .= "$alpha\n";
+				if($gamma > 1){
+					$parameters .= "$fraction\n";
+				}
+			}
+			
+			$parameters .= "$tempDir/weights\n";
+
+			my @sites = split /,/,$weight;
+			my @weights;
+			for my $i(0..$letters-1) {
+				$weights[$i] = 0;
+			}
+			for my $i(0..$#sites) {
+				$weights[$sites[$i] - 1] = 1;
+			}
+			
+			open WEIGHTS, ">$tempDir/weights";
+			print WEIGHTS join "",@weights;
+			close WEIGHTS;
+		}
+		else{
+			$parameters .= "Y\n";
+			
+			if($gamma > 0){
+				$parameters .= "$alpha\n";
+				if($gamma > 1){
+					$parameters .= "$fraction\n";
+				}
+			}
+		}
+		#print $parameters;
+		#exit;
+		open MATRIZDNA, ">".$tempDir."/parameters"
+			  or die "ERROR: Unable open file: $tempDir/parameters.\n$!\n";
+		print MATRIZDNA $parameters;
+		close MATRIZDNA;
+		
+		
+		(system("$pwd/Exe/dnadist < $tempDir/parameters > $tempDir/dnadistlog") == 0
+			and move("outfile","$tempDir/matrix"))
+			or &$_dying;
+	};
+	
+	my $_executeProtDist = sub {
+        my $instance = $_[1];
+        
+        my $pwd = $instance->parameter("PWD");
+	    my $model = $instance->parameter("MODEL");
+	    my $tempDir = $instance->parameter("TEMP");
+	    my $fileIn = $instance->parameter("INPUT");
+	    my $gamma = $instance->parameter("GAMMA");
+	    my $weight = $instance->parameter("WEIGHT");
+	    my $parameters = $fileIn."\n";
+        
+	    my $modelValue = 0;
+		if($model =~ /^pm/) {
+			$modelValue = 1;
+		}
+		elsif($model =~ /^pa/) {
+			$modelValue = 2;
+		}
+		elsif($model =~ /^k/) {
+			$modelValue = 3;
+		}
+		for my $i (1..$modelValue) {
+			$parameters .= "P\n";
+		}
+		
+		if(defined $weight && $weight eq "y") {
+			$parameters .= "W\n";
+		}
+		
+		if($modelValue != 3 && defined $gamma && $gamma eq "y") {
+			$parameters .= "G\n";
+			if($gamma eq "gi") {
+				$parameters .= "G\n";
+			}
+		}
+		
+		$parameters .= "I\n2\nY\n";
+		
+		#print $parameters;
+		#exit;
+
+		open MATRIZPROT, ">".$tempDir."/parameters"
+			   or die "ERROR: Unable open file: $tempDir/parameters.\n$!\n";
+		print MATRIZPROT $parameters;
+		close MATRIZPROT;
+		
+		system "$pwd/Exe/protdist < $tempDir/parameters > $tempDir/protdistlog";
+		move "./outfile", "$tempDir/matrix";
+	};
+	
+	## captures parameters and chooses the ma-	##
+	## trix construction method					##
+	
+	sub executeMatrix {
+   		my $self = shift;
+       	my $instance = Parameters::getInstance;
+       	if($instance->parameter("TYPE") =~ /^d/){	#DNA sequence
+       		$self->$_executeDNADist($instance);
+       	}
+       	else {									#Prot sequence
+       		$self->$_executeProtDist($instance);
+       	}
+	}
+    
+   	1;
---
+++ BasePackage/MainParameters.pm
@@ -0,0 +1,104 @@
+	##################################################
+	## that class stores necessary parameters to	##
+	## all class of the distance's modulate of		##
+	## GRAFU										##
+	##################################################
+
+	package Parameters;
+	use strict;
+	use warnings;
+
+	##################################################
+	## the object constructor						##
+	##################################################
+    
+	my $_new = sub {
+        	my $self  = {};
+        	$self->{"TYPE"} 		= undef;
+        	$self->{"INPUT"} 		= undef;
+        	$self->{"QSPEC"}		= undef;
+			$self->{"ALPHA"}		= undef;
+			$self->{"GAMMA"}		= undef;
+        	$self->{"FREQUE"}		= undef;
+        	$self->{"OUTPUT"} 		= undef;
+        	$self->{"PREFERENCE"}	= undef;
+        	$self->{"LDIFFERENCE"}	= undef;
+        	$self->{"ADDICTIVITY"}	= undef;
+        	$self->{"SEQUENTIAL"}	= undef;
+        	$self->{"LETTER"}		= undef;
+        	$self->{"WEIGHT"} 		= undef;
+        	$self->{"RATIO"} 		= undef;
+        	$self->{"ISITE"}		= undef;	# fraction
+        	$self->{"MODEL"}		= undef;
+        	$self->{"NAMES"}		= undef;
+			$self->{"TEMP"}			= undef;
+			$self->{"PWD"}			= undef;
+			bless($self);
+			return $self;
+	};
+    
+	##################################################
+	## class attribute								##
+	##################################################
+	
+	my $instance = undef;
+	    
+	##################################################
+	## methods to access data						##
+	##################################################
+
+	sub parameter {
+  	 	my $self = shift;
+       	if (@_ == 2) {  $self->{$_[0]}  = $_[1] }
+       	return $self->{$_[0]};
+	}
+    
+   	sub configure {
+   		my $self = shift;
+    	while(@_){
+    		my $indice = shift;
+    		$self->{$indice} = shift; 
+    	}
+		
+		# configura parametros default para parametros não setados ou incorretos
+		$self->{"GAMMA"} = (!defined $self->{"ALPHA"})? 0 : #opcao "n"
+			   ((defined $self->{"ISITE"}) ? 2 : 1);    	#opcao "gi" ou "y"
+		my $type = $self->{"TYPE"};
+       	$self->{"TYPE"}	 = "dna" if(!defined $type or
+        						($type ne "dna"   and
+        					 	 $type ne "prot"));
+        my $preference = $self->{"PREFERENCE"};
+        $self->{"PREFERENCE"}	= "a" if(!defined $preference or
+        								($preference ne "a"  and
+        					 			 $preference ne "ta" and
+        					 			 $preference ne "t"));
+		defined $self->{"MODEL"} or $self->{"MODEL"} = "kimura";
+		defined $self->{"INPUT"} or die "ERRO: Esqueceu de passar o arquivo de entrada.\n";
+        defined $self->{"OUTPUT"} or die "ERRO: Esqueceu de passar o arquivo de saida.\n";
+        	
+        # Demais Parametros:
+        # $self->{"ALPHA"}	 	 = MainAbstract trata default;
+        # $self->{"ISITE"}	 	 = MainAbstract trata default;
+	    # $self->{"QSPEC"}	 	 = setado por Converter;
+	    # $self->{"LDIFFERENCE"} = usado somente por Main;
+	    # $self->{"ADDICTIVITY"} = usado somente por Main;
+        # $self->{"SEQUENTIAL"}  = setado por Converter;
+	    # $self->{"LETTER"}	 	 = setado por Converter;
+	    # $self->{"WEIGHT"} 	 = MainAbstract trata default;
+	    # $self->{"RATIO"} 	 	 = 2.0 default no MainAbstract;
+	    # $self->{"MODEL"}	 	 = F84 default no MainAbstract;
+	    # $self->{"NAMES"}	 	 = usado somente por Converter;
+	    # $self->{"TEMP"}	 	 = setado no Run;
+		# $self->{"PWD"}	 	 = setado no Run;
+	}
+    
+	sub getInstance {
+    	if(!defined $instance) {
+    		my $self = shift;
+    		$instance = $self->$_new();
+    	}
+    	return $instance;
+    }
+
+    1;
+	## main e converter mexem aki
