0ba1d48aefe78913fa83195b5c77ada5004f8d90
Gilmar R. O. Dias <grodias@gmail.com>
2010-02-08 16:18:13 -0300

    adição de um script de cálculos para a validação das árvores

--- Tests/treedist_test.pl
+++ Tests/treedist_test.pl
@@ -1,13 +1,18 @@
 #!/usr/bin/perl -w
 
 @seqnumbers = ("2304", "2636", "2637", "2638", "2639", "2640", "2641");
+@preference = ("a", "t", "ta");
 
 foreach $seqnum (@seqnumbers){
 
-	open TREEDIST, " | ./treedist " || die "erro ao executar treedist";
-	print TREEDIST "m$seqnum.seq_phyml_tree.txt\nd\n2\nc\nV\nY\nm$seqnum.seq_digrafu_tree.txt\n";
-	close TREEDIST;
+	foreach $pref (@preference){
 
-	system("mv outfile out_m$seqnum") && die "erro ao renomear arquivo \"outfile\" $?";
+		open TREEDIST, " | ./treedist " || die "erro ao executar treedist";
+		print TREEDIST "m$seqnum.seq_phyml_tree.txt\n2\nc\nV\nY\nm$seqnum.seq_digrafu_tree_$pref.txt\n";
+		close TREEDIST;
+
+		system("mv outfile out_m$seqnum"."_$pref"."_branchs") && die "erro ao renomear arquivo \"outfile\" $?";
+
+	}
 
 }
