4cd18cdc25ef18705b908640a8f794fd00b8fe07
Gilmar R. O. Dias <grodias@gmail.com>
2010-02-17 17:00:37 -0300

    adaptação do script treeCalcspara validar várias sequências

--- Tests/treeCalcs.pl
+++ Tests/treeCalcs.pl
@@ -2,20 +2,26 @@
 
 use strict;
 
-my $dna_dir = "../Sequencias/reais_dna";
-my $prot_dir = "../Sequencias/reais_prot";
-my $out_dir = "validação2";
-my $tree_dir = "";
+# Colocar os caminhos dos diretórios que serão usados na validação
+# (obs: caminhos relativos a pasta digrafu/Test, onde deve estar localizado este arquivo)
+my $dna_dir = "sequencias/dna";
+my $prot_dir = "sequencias/prot";
+my $out_dir = "validacao3";
+my $tree_dir = "trees";
+my $phyml_dir = "PhyML_3.0";
+
+# Arquivo com os resultados obtidos na validação
+my $tabela = "resultados";
 
 my @dna_sequences = split /\n/, `ls $dna_dir`;
 my @prot_sequences = split /\n/, `ls $prot_dir`;
-my @tree_files = split /\n/, `ls $tree_dir`;
-my @preference = ("a", "t", "ta");
+my @preference = ("a", "ta", "t");
 
 my $option;
 my $file;
 my $tree;
 my $pref;
+
 
 Main:
 
@@ -25,13 +31,13 @@ Main:
 	foreach $option (@ARGV){
 
 		if($option eq "1"){
-			gera_arvores_digrafu();
+			gera_arvores();
 		}
 		elsif($option eq "2"){
 			compara_arvores();
 		}
 		elsif($option eq "3"){
-			conta_galhos();
+			cria_arquivos_arvores();
 		}
 		elsif($option eq "4"){
 			calcula_medias();
@@ -53,22 +59,84 @@ Main:
 
 
 #################################################################################
-## Executa o DiGrafu com as sequências em $dna_dir e $prot_dir		       ##
+## Executa o DiGrafu com as sequências em $dna_dir e $prot_dir.		       ##
+## São geradas árvores utilizando os três valores possíveis para o parâmetro   ##
+## PREFERENCE e coloca os arquivos em $tree_dir				       ##
 #################################################################################
-sub gera_arvores_digrafu{
+sub gera_arvores{
 
-	foreach $pref (@preference){
-		# DNA
-		foreach $file(@dna_sequences){
-				system("../Run.pl INPUT $dna_dir/$file OUTPUT  $out_dir/$tree_dir/$file"."_digrafu_tree_$pref.txt PREFERENCE $pref")
-				&& die "erro ao executar o DiGrafu $?\n";
+	# DNA		(obs: todos os arquivos devem ter o mesmo formato -> phylip sequencial | intercalado)
+	foreach $file (@dna_sequences){
+
+		# PhyML
+		system("./$phyml_dir/PhyML_3.0_linux32 -i $dna_dir/$file -d nt -q -m K80 -v e -t e -c 4 -a e -o tlr")
+		&& die "erro ao executar o PhyML $?\n";
+		system("mv $dna_dir/$file"."_phyml* $out_dir/$tree_dir") && die "erro ao mover árvore gerada pelo PhyML $?\n";
+
+		# DiGrafu
+		foreach $pref (@preference){
+			system("../Run.pl INPUT $dna_dir/$file OUTPUT $out_dir/$tree_dir/$file"."_digrafu_tree_$pref.txt PREFERENCE $pref TYPE dna MODEL kimura")
+			&& die "erro ao executar o DiGrafu $?\n";
 		}
-		# Proteína
-		foreach $file(@dna_sequences){
-				system("../Run.pl INPUT $prot_dir/$file OUTPUT  $out_dir/$tree_dir/$file"."_digrafu_tree_$pref.txt PREFERENCE $pref MODEL jtt TYPE prot")
-				&& die "erro ao executar o DiGrafu $?\n";
-		}
+
 	}
+	# Proteína	(obs: todos os arquivos devem ter o mesmo formato -> phylip sequencial | intercalado)
+	foreach $file (@prot_sequences){
+
+		# PhyML
+		system("./$phyml_dir/PhyML_3.0_linux32 -i $prot_dir/$file -d aa -m JTT -v e -c 4 -a e -o tlr")
+		&& die "erro ao executar o PhyML $?\n";
+		system("mv $prot_dir/$file"."_phyml* $out_dir/$tree_dir") && die "erro ao mover árvore gerada pelo PhyML $?\n";
+
+		# DiGrafu
+		foreach $pref (@preference){
+			system("../Run.pl INPUT $prot_dir/$file OUTPUT  $out_dir/$tree_dir/$file"."_digrafu_tree_$pref.txt PREFERENCE $pref TYPE prot MODEL jtt")
+			&& die "erro ao executar o DiGrafu $?\n";
+		}
+
+	}
+
+}
+
+
+#################################################################################
+## Cria um arquivo com todas as árvores para ser			       ##
+## usado no Treedist e no treeUtils					       ##
+#################################################################################
+sub cria_arquivos_arvores{
+
+	my @phyml_trees = split /\n/, `ls $out_dir/$tree_dir/*_phyml_tree.txt`; # OBS: os nomes dos arquivos ficarão com os nomes das pastas
+	my @digrafu_trees;
+	local $/;
+
+	foreach my $phyml_tree (@phyml_trees){
+
+		$phyml_tree =~ /$out_dir\/$tree_dir\/(.+)_phyml_tree.txt/;
+		open TREEFILE, ">$out_dir/$tree_dir/all_trees_$1.txt" or die "erro ao abrir arquivo de árvores $?\n";
+		my $filename = $1."_digrafu_tree_*.txt";
+		@digrafu_trees = split /\n/, `ls $out_dir/$tree_dir/$filename`;
+
+
+		foreach my $digrafu_tree (@digrafu_trees){
+			open PHYML, "$phyml_tree";
+			$_ = <PHYML>;
+			s/[\s]*//g;
+			print TREEFILE "$_\n";
+
+			open DIGRAFU, "$digrafu_tree";
+			$_ = <DIGRAFU>;
+			s/[\s]*//g;
+			print TREEFILE "$_\n";
+		
+			close DIGRAFU;
+			close PHYML;
+
+		}
+
+		close TREEFILE;
+
+	}
+
 
 }
 
@@ -79,38 +147,36 @@ sub gera_arvores_digrafu{
 #################################################################################
 sub compara_arvores{
 
-	foreach $tree (@tree_files){
+	cria_arquivos_arvores();
 
-		if($tree =~ /(.+)_digrafu_tree_/){
+	#system("rm outfile") && die "erro ao remover arquivo \"outfile\" $?";
 
-			my $original_tree = $1."_phyml_tree.txt";
+	my @all_tree_files = `ls $out_dir/$tree_dir/all_trees_*.txt`;
 
-			foreach $pref (@preference){
+	foreach my $tree_file (@all_tree_files){
+		
+		$tree_file =~ /$out_dir\/$tree_dir\/all_trees_(.+).txt/;
+		my $filename = $1;
 
-				# Symetric Difference
-				open TREEDIST, " | ./treedist " || die "erro ao executar treedist";
-				print TREEDIST "$out_dir/$tree_dir/$original_tree\nd\n2\nc\nV\nY\n$out_dir/$tree_dir/$tree\n";
-				close TREEDIST;
+		# Symetric Difference
+		open TREEDIST, " | ./treedist " || die "erro ao executar o Treedist $?\n";
+		print TREEDIST "$tree_file"."D\n2\nA\nS\nY\n";
+		close TREEDIST;
 
-				system("mv outfile $out_dir/out_$tree") && die "erro ao renomear arquivo \"outfile\" $?";
+		system("mv outfile $out_dir/dist_trees_$filename.txt") && die "erro ao renomear arquivo \"outfile\" $?";
 
-				# Branch Score Distance
-				open TREEDIST, " | ./treedist " || die "erro ao executar treedist";
-				print TREEDIST "$out_dir/$tree_dir/$original_tree\n2\nc\nV\nY\n$out_dir/$tree_dir/$tree\n";
-				close TREEDIST;
+		# Branch Score Distance
+		open TREEDIST, " | ./treedist " || die "erro ao executar o Treedist $?\n";
+		print TREEDIST "$tree_file"."2\nA\nS\nY\n";
+		close TREEDIST;
 
-				system("mv outfile $out_dir/out_$tree"."_branchs") && die "erro ao renomear arquivo \"outfile\" $?";
+		system("mv outfile $out_dir/dist_trees_$filename"."_branchs.txt") && die "erro ao renomear arquivo \"outfile\" $?";
 
-			}
-
-		}
+		calcula_medias($filename);
 
 	}
 
 }
-
-
-sub conta_galhos{}
 
 
 #################################################################################
@@ -118,6 +184,42 @@ sub conta_galhos{}
 ## e da árvore original gerada pelo PhyML				       ##
 #################################################################################
 sub calcula_medias{
+
+	my $treefile = shift;
+	my $phymlavg;
+	my $digrafuavg;
+	my $media;
+	my $line;
+	my $branches;
+
+	open BRANCHES, "./treeUtils.pl -f $out_dir/$tree_dir/all_trees_$treefile.txt -u 1 |" || die "erro ao executar \"treeUtils.pl -f $out_dir/$tree_dir/all_trees_$treefile.txt -u 1\"\n";
+	open AVG, ">>$out_dir/medias" or die "falha ao abrir arquivo de médias";
+
+	print AVG "$treefile\t";
+
+	foreach $pref (@preference){
+		
+		$line = <BRANCHES>;
+		$line =~ /^(.+) (.+) (.+) (.+)$/;
+		$phymlavg = $4/$3;
+		$line = <BRANCHES>;
+		$line =~ /^(.+) (.+) (.+) (.+)$/;
+		$digrafuavg = $4/$3;
+		$media = ($phymlavg+$digrafuavg)/2.0;
+
+		print AVG "\t$media";
+		$branches = $3;
+	}
+
+	print AVG "\t$branches\n";
+
+	close AVG;
+	close BRANCHES;
+
+}
+
+
+sub calcula_medias_old{
 
 	my $line;
 
@@ -149,42 +251,87 @@ sub calcula_medias{
 #################################################################################
 sub padroniza_valores{
 
-	my @symetric_results = (0, 0, 0);
-	my @branch_results = (0.33, 0.33, 0.31);
-	my $numspecies = 10;
-	my $qtgalhos = 2*$numspecies-3;
+	my $max_qt;
+	my @max_qt_br;
 	my $maxvalue;
-	my @maxvalues;
-	my @finalvalues;
-	my $line;
-	my $num = 0;
+	my $result;
+	my $line2;
+
+	open TAB, ">$out_dir/$tabela" or die "erro ao abrir arquivo de resultados finais\n";
+
+	open AVG, "$out_dir/medias" or die "erro ao abrir arquivo de médias\n";
+	foreach my $line (<AVG>){
+
+		if($line =~ /^(\S+)\s+(\S+)\s+(\S+)\s(\S+)\s+(\S+)$/){
+
+			my $qt;
+			my $seq = $1;
+			my $media_a = $2;
+			my $media_ta = $3;
+			my $media_t = $4;
+			my $nbranches = $5;
+			my $type;
+
+			my $seqdir;
+			foreach my $sequence (@dna_sequences){
+				if($seq eq $sequence){
+					$seqdir = $dna_dir;
+					$type = "dna";
+				}
+				else{
+					$seqdir = $prot_dir;
+					$type = "proteina";
+				}
+			}
+			open SEQ, "$seqdir/$seq" or die "erro ao abrir arquivo de sequência\n";
+
+			my $info = <SEQ>;
+			$info =~ /(\d+)\s+(\d+)/;
+			my $species = $1;
+			my $sites = $2;
+			close SEQ;
+
+			$qt = (2*$species-3);
+			print TAB "$seq\t$type\t$species\t$sites\t$nbranches\t";
+
+			# Topologica
+			open SD, "$out_dir/dist_trees_$seq.txt" or die "erro ao abrir arquivo de topologias\n";
+			foreach my $line (<SD>){
+
+				$line =~ /\s(\S+)$/;
+				$result = $1;
+				$maxvalue = 2*$qt;
+
+				print TAB ( "$result\t".(($maxvalue-$result)/$maxvalue)."\t" );
 
 
-	# topologica
-	print "Diferenças simétricas:\n";
-	$maxvalue = 2*$qtgalhos;
-	foreach my $result (@symetric_results){
-		print( (($maxvalue-$result)/$maxvalue)."\n" );
+			}
+			close SD;
+
+			open BSD, "$out_dir/dist_trees_$seq"."_branchs.txt" or die "erro ao abrir arquivo de galhos\n";
+			# Galhos
+			$line2 = <BSD>;
+			$line2 =~ /.+\s(\S+)$/;
+			$result = $1+0;
+			$maxvalue = 2*$qt*$media_a;
+			print TAB ( "$result\t".(($maxvalue-$result)/$maxvalue)."\t" );
+			$maxvalue = 2*$qt*$media_ta;
+			print TAB ( "$result\t".(($maxvalue-$result)/$maxvalue)."\t" );
+			$maxvalue = 2*$qt*$media_t;
+			print TAB ( "$result\t".(($maxvalue-$result)/$maxvalue)."\n" );
+
+			close BSD;
+
+		}
+
 	}
-
-	open MEDIAS, "$out_dir/medias" or die "erro ao abrir arquivo de medias\n";
-	while ($line = <MEDIAS>){
-		$line =~ /^(.+) (.+) (.+) (.+)$/;
-
-		my $media = $4;
-		push @maxvalues, 2*$qtgalhos*$media;
-
-	}
-	close MEDIAS;
-
-	# galhos
-	print "\nDistâncias entre galhos:\n";
-	while($num <= $#branch_results){
-		push( @finalvalues, (($maxvalues[$num])-$branch_results[$num])/($maxvalues[$num]) );
-		print (($num+1)."  =>  ".$finalvalues[$num++]."\n");
-	}
+	close AVG;
+	close TAB;
 
 }
+
+
+sub clean{}
 
 
 #################################################################################
